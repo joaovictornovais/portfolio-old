edd13145615cdbc9c2779923d401fe5184bf934b
João Victor <joao.dipaay@gmail.com>
2023-06-16 16:29:07 -0300

    mobile: contact section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     <section id="about" className='bg-white h-full flex p-8 py-24 flex-col gap-4'>
       <img className='rounded-xl' 
         src="https://wallpaperaccess.com/full/8077555.png" alt="ANIME GIRL PROGRAMMING" />
-      <h2 className='text-blue-600 font-bold uppercase text-center pt-4 text-xl'>About me</h2>
-      <h3 className='text-2xl font-bold text-center text-gray-800'>A dedicated Full-Stack Developer based in São Paulo, Brazil 📍</h3>
+      <h3 className='text-blue-600 font-bold uppercase text-center pt-4 text-xl'>About me</h3>
+      <h2 className='text-2xl font-bold text-center text-gray-800'>A dedicated Full-Stack Developer based in São Paulo, Brazil 📍</h2>
       <p className='text-center text-lg text-zinc-600'>As a internship Full-Stack Developer, I possess an impressive arsenal of front-end skills in HTML, CSS, JavaScript, React and TailwindCSS. As a back-end developer, I have a solid knowledge of Java, Python, PostgreSQL and Firebase. 
       <p>Isn't just my job, programming is also one of my favorites hobbies that a practice a lot everyday.</p>
       <p>I really like to learn and teach code stuffs to my discord friends.</p></p>
---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section id="contact" className="bg-white flex justify-center items-center h-full p-8 py-24">
+      <div className='flex flex-col gap-4'>
+        <h3 className='text-blue-600 font-bold uppercase text-center pt-4 text-xl'>Contact</h3>
+        <h2 className='font-bold text-gray-800 text-2xl text-center'>Don't be shy! Hit me up! 👇</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
