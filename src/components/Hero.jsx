83c064e89a5ae6ea43b3f9bb7e4add398681fd70
João Victor <joao.dipaay@gmail.com>
2023-06-18 20:02:03 -0300

    feat: skills clean css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,24 +4,28 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <section id="hero" className='hero-container'>
+    <section id="hero"
+      className='hero-container'>
       <div className='p-8'>
         <img className='w-[300px] rounded-xl'
           src="https://i.pinimg.com/736x/02/11/aa/0211aa42dd92331ff9e3f9a06f13bab5.jpg"
           alt="joão victor" />
       </div>
-      
       <div className='center-container'>
-        <h1 className='hero-title'>Full-Stack Java/React Developer 🌊</h1>
-        <p className='paragraph max-w-[500px]'>Hi I'm João Victor. A passionate Full-stack Java/React Developer based in São Paulo, Brazil. 📍</p>
+        <h1 className='hero-title'>
+          Full-Stack Java/React Developer 🌊
+        </h1>
+        <p className='paragraph max-w-[500px]'>
+          Hi I'm João Victor. A passionate Full-stack Java/React Developer based in São Paulo, Brazil. 📍
+        </p>
 
         <div className='center text-gray-800'>
           <a className='blue-hover hover:scale-105'
             href='https://www.github.com/joaovictornovais' target="__blank">
-              <FaGithub size={40} /></a>
+            <FaGithub size={40} /></a>
           <a className='blue-hover hover:scale-105'
             href='https://www.linkedin.com/in/joaovictornovais' target="__blank">
-              <FaLinkedin size={40} /></a>
+            <FaLinkedin size={40} /></a>
         </div>
       </div>
     </section>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,13 +4,13 @@ const Skills = () => {
     const frontend = ['html5', 'css3', 'javascript', 'react', 'tailwind']
     const backend = ['java', 'python', 'postgresql', 'firebase']
     return (
-        <div className='flex flex-col gap-4 items-center p-4 bg-zinc-100 py-16'>
-            <h2 className='font-bold text-xl text-gray-800 border-b-2 border-gray-800 py-1 px-2'>Skills</h2>
-            <div className='flex flex-col gap-4'>
+        <div className='skills-container'>
+            <h2 className='skills-title'>Skills</h2>
+            <div className='column'>
                 <ul className='flex gap-4'>
                     {
                         frontend.map((stack) => (
-                            <li className='bg-white p-3 rounded-full shadow-xl flex justify-center items-center hover:scale-105 transition-all duration-200 cursor-pointer'>
+                            <li className='skills-circle'>
                                 <img className='w-[50px]'
                                     src={stack != 'tailwind' ? `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${stack}/${stack}-original.svg` : "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"} />
                             </li>
@@ -18,11 +18,11 @@ const Skills = () => {
                     }
                 </ul>
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='column'>
                 <ul className='flex gap-4'>
                     {
                         backend.map((stack) => (
-                            <li className='bg-white p-2 rounded-full shadow-xl flex justify-center items-center hover:scale-105 transition-all duration-200 cursor-pointer'>
+                            <li className='skills-circle'>
                                 <img className='w-[50px]'
                                     src={stack != 'firebase' ? `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${stack}/${stack}-original.svg` : "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"} />
                             </li>
