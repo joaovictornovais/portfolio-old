77d1196e5a9ecc1c917eb97301f223d04cd582d9
João Victor <joao.dipaay@gmail.com>
2023-06-18 20:10:21 -0300

    feat: contact + projects clean css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,23 +5,23 @@ import { TbMailOpened } from 'react-icons/tb'
 
 const Contact = () => {
   return (
-    <section id="contact" className="bg-white flex justify-center items-center h-full p-8 py-24">
+    <section id="contact" className="contact-container">
       <div className='flex flex-col gap-6'>
-        <h3 className='text-blue-600 font-bold uppercase text-center text-xl'>Contact</h3>
-        <h2 className='font-bold text-gray-800 text-2xl text-center'>Don't be shy! Hit me up! 👇</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-12 pt-6'>
-          <div className='flex flex-col gap-2 justify-center items-center'>
-            <div className='bg-white p-4 rounded-full shadow-xl text-blue-600 hover:cursor-pointer hover:scale-105 duration-200'>
+        <h3 className='section-topic'>Contact</h3>
+        <h2 className='section-title'>Don't be shy! Hit me up! 👇</h2>
+        <div className='contact-grid'>
+          <div className='contact-circle'>
+            <div className='contact-item'>
               <FaMapMarkedAlt size={40} />
             </div>
-            <h3 className='text-xl font-bold text-gray-800'>Location</h3>
+            <h3 className='contact-legend'>Location</h3>
             <p className='text-gray-700'>São Paulo, Brazil</p>
           </div>
-          <div className='flex flex-col gap-2 justify-center items-center'>
-            <div className='bg-white p-4 rounded-full shadow-xl text-blue-600 hover:cursor-pointer hover:scale-105 duration-200'>
+          <div className='contact-circle'>
+            <div className='contact-item'>
               <TbMailOpened size={40} />
             </div>
-            <h3 className='text-xl font-bold text-gray-800'>E-mail</h3>
+            <h3 className='contact-legend'>E-mail</h3>
             <p className='text-gray-700'>joaovkt.novais@gmail.com</p>
           </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <section id="projects" className='h-full bg-zinc-100 flex justify-center items-center p-8 py-24'>
-      <h2 className='text-3xl text-center font-bold text-gray-800'>New projects coming soon...🤠</h2>
+    <section id="projects" className='projects-container'>
+      <h2 className='section-title'>New projects coming soon...🤠</h2>
     </section>
   )
 }
